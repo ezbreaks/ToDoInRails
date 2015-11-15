8bf01f7e44c17f3c8ac3bf5590b662c547f2c7b2
Chirvase Dragos <chirvase.dragos.andrei@gmail.com>
2015-11-16 00:57:55 +0200

    Added Task Validation and Image / soundcloud previewer

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,22 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
 
+	validates :user_id, presence: true
+	validates :content, presence: true
+
 	auto_html_for :content do 
 		html_escape
 		image
+		soundcloud
 		youtube(:width => '100%', :height => 250, :autplay => false)
 		link :target => '_blank', :rel => 'nofollow'
 		simple_format
+
+	end
+
+	AutoHtml.add_filter(:image) do |text|
+	  text.gsub(/http:\/\/.+\.(jpg|jpeg|bmp|gif|png)(\?\S+)?/i) do |match|
+	    %|<img src="#{match}" alt=""/>|
+	  end
 	end
 end
